c5c842be25dbe4a2b1b34f6cdf614735efeafd48
Cameron Hardy <camhardy99@gmail.com>
2021-09-18 16:01:54 -0500

    Removed unused constructor - moved checkboxes to Type and Weakness functions

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,41 +1,27 @@
 import React from "react";
 import "./Navbar.css";
 import Type from "./Type"
+import Weakness from "./Weakness"
 
 class Navbar extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            pokemon: props.pokemon
-        }
-    }
     render() {
-        console.log(this.state.pokemon)
         return (
             <nav>
                 <ul>
+                    <li><h1>Pokédex</h1></li>
                     <li>
                         <input data-key="search" className="search" placeholder="Pokemon name..." type="text" />
                     </li>
                     <li>
                         <details id="type">
                             <summary>Type</summary>
-                            <Type pokemon={this.state.pokemon}/>
+                            {Type(this.props.pokemon)}
                         </details>
                     </li>
                     <li>
                         <details id="weakness">
                             <summary>Weakness</summary>
-                            <ul>
-                                <li>
-                                    <input data-key="weakness" className="option" type="checkbox" name="ice" />
-                                    Ice
-                                </li>
-                                <li>
-                                    <input data-key="weakness" className="option" type="checkbox" name="rock" />
-                                    Rock
-                                </li>
-                            </ul>
+                            {Weakness(this.props.pokemon)}
                         </details>
                     </li>
                 </ul>
