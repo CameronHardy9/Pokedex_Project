dcb6d797e594f3fbe9ad1452b232fb22c93a5caf
Cameron Hardy <camhardy99@gmail.com>
2021-09-18 16:04:53 -0500

    Attempted to add checkbox search filter - still not working - only search is functional

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,18 +3,23 @@ import "./Card.css"
 function Card(props) {
     let items = props.pokemon.map((item, idx) => {
         let { type, name, id, img, num, weaknesses} = item;
-        if (name.includes(props.search) || !props.search)
-            return (
-                <li key={idx} id={id} className="pokeCardContainer">
-                    <div className="pokeCard">
-                        <img src={img} alt={name} />
-                        <div>{num}</div>
-                        <div>{name}</div>
-                        <div>{type}</div>
-                        <div>{weaknesses}</div>
-                    </div>
-                </li>
-            );
+        if (name.includes(props.search) || !props.search) {
+            if(!props.type[0] || props.type.filter((i) => {type.includes(i)})) {        //Does not work
+                if(!props.weakness[0] || weaknesses.forEach((e) => {props.weakness.includes(e)})) {     //Does not work
+                    return (
+                        <li key={idx} id={id} className="pokeCardContainer">
+                            <div className="pokeCard">
+                                <img className="profilePic" src={img} alt={name} />
+                                <div>{num}</div>
+                                <div>{name}</div>
+                                <div>{type}</div>
+                                <div>{weaknesses}</div>
+                            </div>
+                        </li>
+                    );
+                }
+            }
+        }
     });
     return <ul className="allCards">{items}</ul>;
 }
