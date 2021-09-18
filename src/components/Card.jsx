83daf774ef1067bf162b1df78235b709f76514c3
Cameron Hardy <camhardy99@gmail.com>
2021-09-18 00:04:17 -0500

    added image and extra data to each pokeCard

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,17 @@
+import "./Card.css"
+
 function Card(props) {
     let items = props.pokemon.map((item, idx) => {
-        let { type, name, id } = item;
+        let { type, name, id, img, num, weaknesses} = item;
         if (name.includes(props.search) || !props.search)
             return (
                 <li key={idx} id={id} className="pokeCardContainer">
                     <div className="pokeCard">
-                        <span>{name}</span>
-                        <span>{type}</span>
+                        <img src={img} alt={name} />
+                        <div>{num}</div>
+                        <div>{name}</div>
+                        <div>{type}</div>
+                        <div>{weaknesses}</div>
                     </div>
                 </li>
             );
