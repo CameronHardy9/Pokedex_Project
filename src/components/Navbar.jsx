016f82f6cf13259c8a74fe41e807873b4d310182
Cameron Hardy <camhardy99@gmail.com>
2021-09-21 00:35:49 -0500

    Added randomized search suggestion feature

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,13 +4,26 @@ import Type from "./Type"
 import Weakness from "./Weakness"
 
 class Navbar extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            suggestion: "Pokémon name...",
+        }
+    }
+    componentDidMount() {
+        setInterval(this.searchSuggestion, 3000);
+    }
+    searchSuggestion = () => {
+        let rand = Math.round(Math.random() * this.props.pokemon.length);
+        this.setState({suggestion: this.props.pokemon[rand].name});
+    }
     render() {
         return (
             <nav>
                 <ul>
                     <li><h1 className="pokedex">Pokédex</h1></li>
                     <li>
-                        <input data-key="search" className="search" placeholder="Pokémon name..." type="text" />
+                        <input data-key="search" className="search" placeholder={this.state.suggestion} type="text" />
                     </li>
                     <li>
                         <details id="type">
