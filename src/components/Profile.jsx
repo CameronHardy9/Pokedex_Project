00a26de49ccff17809f213e0c772d949f895fbea
Cameron Hardy <camhardy99@gmail.com>
2021-09-22 12:03:58 -0500

    Updated styling for individual profile pages

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -6,24 +6,31 @@ function Profile({ profile }) {
             <div className="upperProfile">
                 <h1>{profile.name}</h1>
                 <img src={profile.img} alt=""></img>
-                <div>#{profile.num}</div>
-                <div ><strong>{profile.name}</strong></div>
+                <div className="charNum">#{profile.num}</div>
             </div>
             <div className="midProfile">
-                <label><strong>Height</strong></label>
-                <div>{profile.height}</div>
-                <label><strong>Weight</strong></label>
-                <div>{profile.weight}</div>
+                <div>
+                    <label className="profileLabel">Height</label>
+                    <div className="stat">{profile.height}</div>
+                </div>
+                <div>
+                    <label className="profileLabel">Weight</label>
+                    <div className="stat">{profile.weight}</div>
+                </div>
             </div>
             <div className="lowerProfile">
-                <label><strong>Type</strong></label>
-                <ul>
-                    {profile.type.map((item, idx) => <li key={idx}>{item}</li>)}
-                </ul>
-                <label><strong>Weaknesses</strong></label>
-                <ul>
-                    {profile.weaknesses.map((item, idx) => <li key={idx}>{item}</li>)}
-                </ul>
+                <div>
+                    <label className="profileLabel">Type</label>
+                    <ul>
+                        {profile.type.map((item, idx) => <li className="stat" key={idx}>{item}</li>)}
+                    </ul>
+                </div>
+                <div>
+                    <label className="profileLabel">Weaknesses</label>
+                    <ul>
+                        {profile.weaknesses.map((item, idx) => <li className="stat" key={idx}>{item}</li>)}
+                    </ul>
+                </div>
             </div>
         </div>
     );
